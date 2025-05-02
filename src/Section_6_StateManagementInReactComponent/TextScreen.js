b8f4773888cbc955cdf0aa80266b39f817f8c883
Vinayak Soni <vinayaksoni106@gmail.com>
2025-05-02 17:56:43 +0530

    Completed Section 6

---
+++ src/Section_6_StateManagementInReactComponent/TextScreen.js
@@ -0,0 +1,40 @@
+import {Text, TextInput, View} from "react-native";
+import {useState} from "react";
+
+const TextScreen = ()=>{
+    const [name, setName] = useState('')
+    const [password, setPassword] = useState('')
+    return (
+        <View>
+            <Text>Enter Name: </Text>
+            <TextInput
+                style={styles.input}
+                autoCapitalize={'none'}
+                autoCorrect={false}
+                value={name}
+                onChangeText={(newValue)=>setName(newValue)}
+            />
+            <Text>My name is {name}</Text>
+
+            <Text>Enter Password: </Text>
+            <TextInput
+                style={styles.input}
+                autoCapitalize={'none'}
+                autoCorrect={false}
+                value={password}
+                onChangeText={(newValue)=>setPassword(newValue)}
+            />
+            {password?.length < 4 ? <Text>Password must be 4 characters</Text>:null}
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    input:{
+        margin:15,
+        borderColor:'black',
+        borderWidth:1,
+    }
+})
+
+export default TextScreen
